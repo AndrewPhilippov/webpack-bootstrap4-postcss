8ec20c5648e5b61bc604178f9b4d99240778925e
Andrew <a@fandb.pro>
2020-03-03 08:35:15 +0300

    Smooth nav functionality added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -10,6 +10,7 @@ new StickyHeader()
 // new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 // new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 new MobileMenu()
+new SmoothNav()
 let modal
 
 console.log('Привет')
--- app/assets/scripts/modules/SmoothNav.js
+++ app/assets/scripts/modules/SmoothNav.js
@@ -1,51 +1,44 @@
 class SmoothNav {
     constructor () {
-        this.triggers = document.querySelectorAll('.list--smooth > li')
-        this.background = document.querySelector('.dropdownBackground')
-        this.nav = document.querySelector('.header__nav')
+        this.triggers = document.querySelectorAll('.list--smooth > li');
+        this.background = document.querySelector('.dropdownBackground');
+        this.nav = document.querySelector('.header__nav');
         this.events()
     }
 
     events () {
-        this.triggers.forEach(trigger => trigger.addEventListener('mouseenter', this.handleEnter))
-        this.triggers.forEach(trigger => trigger.addEventListener('mouseleave', this.handleLeave))
+        const self = this
+        this.triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter))
+        this.triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave))
+
+        function handleEnter () {
+            this.classList.add('trigger-enter')
+            setTimeout(() => {this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active')}, 150)
+            self.background.classList.add('open')
+
+            const dropdown = this.querySelector('.dropdown')
+            const dropdownCoords = dropdown.getBoundingClientRect()
+            const navCoords = self.nav.getBoundingClientRect()
+
+            const coords = {
+                height: dropdownCoords.height,
+                width: dropdownCoords.width,
+                top: dropdownCoords.top - navCoords.top,
+                left: dropdownCoords.left - navCoords.left
+            };
+            self.background.style.setProperty('width', `${coords.width}px`);
+            self.background.style.setProperty('height', `${coords.height}px`);
+            self.background.style.setProperty('right', `${coords.right}px`);
+            self.background.style.setProperty('left', `${coords.left}px`);
+            self.background.style.setProperty('top', `${coords.top}px`);
+            // self.background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+        }
+        function handleLeave () {
+            this.classList.remove('trigger-enter', 'trigger-enter-active');
+            self.background.classList.remove('open')
+        }
+
     }
-
-    getSIze (el) {
-        const {x, y, height, width} = el.getBoundingClientRect();
-        console.log(x, y, height, width);
-    }
-
-    handleEnter () {
-        this.classList.add('trigger-enter');
-        setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
-        this.background.classList.add('open')
-
-        const dropdown = this.querySelector('.dropdown');
-        const dropdownCoords = dropdown.getBoundingClientRect();
-        const navCoords = nav.getBoundingClientRect();
-
-        const coords = {
-            height: dropdownCoords.height,
-            width: dropdownCoords.width,
-            top: dropdownCoords.top - navCoords.top,
-            left: dropdownCoords.left - navCoords.left,
-            right: dropdownCoords.right + navCoords.right,
-        };
-
-        this.background.style.setProperty('width', `${coords.width}px`)
-        this.background.style.setProperty('height', `${coords.height}px`)
-        this.background.style.setProperty('right', `${coords.right}px`)
-        this.background.style.setProperty('left', `${coords.left}px`)
-        this.background.style.setProperty('top', `${coords.top}px`)
-        this.background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`)
-    }
-
-    handleLeave () {
-        this.classList.remove('trigger-enter', 'trigger-enter-active');
-        this.background.classList.remove('open')
-    }
-
 }
 
 export default SmoothNav
